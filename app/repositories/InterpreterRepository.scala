33185225e22131243df3d5b329d500b16df23c98
chepurnovs <sergey.chepurnov@nrg-soft.com>
2018-04-07 12:41:38 +0300

    compile script

--- app/repositories/InterpreterRepository.scala
+++ app/repositories/InterpreterRepository.scala
@@ -2,26 +2,59 @@ package repositories
 
 import javax.inject.Singleton
 import play.api.Logger
-
+import scorex.crypto.hash.Blake2b256
+import sigmastate.Values.ByteArrayConstant
+import sigmastate.utxo.ErgoInterpreter
+import test.sigmastate.utxo.ErgoProvingInterpreter
 
 @Singleton
 class InterpreterRepository() {
   private val logger = Logger(this.getClass)
 
-  var env = Map[String, Any](
-    "height1" -> 100000,
-    "height2" -> 50000,
-  )
+  val proverA = new ErgoProvingInterpreter
+  val proverB = new ErgoProvingInterpreter
+  val pubkeyA = proverA.dlogSecrets.head.publicImage
+  val pubkeyB = proverB.dlogSecrets.head.publicImage
+  val verifier = new ErgoInterpreter
+
+  val x = proverA.contextExtenders(1).value.asInstanceOf[Array[Byte]]
+  val hx = ByteArrayConstant(Blake2b256(x))
+
+  val height1 = 100000
+  val height2 = 50000
+
+  val deadlineA = 1000
+  val deadlineB = 500
+  var env = Map(
+    "height1" -> height1,
+    "height2" -> height2,
+    "deadlineA" -> deadlineA,
+    "deadlineB" -> deadlineB,
+    "pubkeyA" -> pubkeyA,
+    "pubkeyB" -> pubkeyB,
+    "hx" -> hx)
 
   def list: Map[String, Any] = {
     env
   }
 
-  def save(key: String, value: Int): Unit = {
+  def get = {
+    Seq(
+      "height1" -> height1,
+      "height2" -> height2,
+      "deadlineA" -> deadlineA,
+      "deadlineB" -> deadlineB,
+      "pubkeyA" -> pubkeyA,
+      "pubkeyB" -> pubkeyB,
+      "hx" -> hx)
+  }
+
+  def save(key: String, value: Any): Unit = {
     env += (key -> value)
   }
 
-  def remove(key: String, value: Int) : Unit = {
-    //env -= (key -> value)
+  def clear() : Unit = {
+    env = Map()
   }
+
 }
