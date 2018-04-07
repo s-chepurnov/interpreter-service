70feb81e26e59704810ac51c43c8eb7b7a1f98a7
chepurnovs <sergey.chepurnov@nrg-soft.com>
2018-04-07 18:57:39 +0300

    context(atomic, ring, demurrage)

--- app/repositories/InterpreterRepository.scala
+++ app/repositories/InterpreterRepository.scala
@@ -67,52 +67,34 @@ class InterpreterRepository() {
   def getRing() = {
     val proverA = new ErgoProvingInterpreter
     val proverB = new ErgoProvingInterpreter
-    val pubkeyA = proverA.dlogSecrets.head.publicImage
-    val pubkeyB = proverB.dlogSecrets.head.publicImage
+    val proverC = new ErgoProvingInterpreter
     val verifier = new ErgoInterpreter
 
-    val x = proverA.contextExtenders(1).value.asInstanceOf[Array[Byte]]
-    val hx = ByteArrayConstant(Blake2b256(x))
+    val pubkeyA = proverA.dlogSecrets.head.publicImage
+    val pubkeyB = proverB.dlogSecrets.head.publicImage
 
-    val height1 = 100000
-    val height2 = 50000
-
-    val deadlineA = 1000
-    val deadlineB = 500
-
-    Map[String, Any](
-      "height1" -> height1,
-      "height2" -> height2,
-      "deadlineA" -> deadlineA,
-      "deadlineB" -> deadlineB,
-      "pubkeyA" -> pubkeyA,
-      "pubkeyB" -> pubkeyB,
-      "hx" -> hx)
+    val env = Map("pubkeyA" -> pubkeyA, "pubkeyB" -> pubkeyB)
+    env
+    //val compiledProp = compile(env, """pubkeyA || pubkeyB""")
   }
 
   def getDemurrage() = {
-    val proverA = new ErgoProvingInterpreter
-    val proverB = new ErgoProvingInterpreter
-    val pubkeyA = proverA.dlogSecrets.head.publicImage
-    val pubkeyB = proverB.dlogSecrets.head.publicImage
+    val demurragePeriod = 100
+    val demurrageCost = 2
+
+    //a blockchain node veryfing a block containing a spending transaction
     val verifier = new ErgoInterpreter
 
-    val x = proverA.contextExtenders(1).value.asInstanceOf[Array[Byte]]
-    val hx = ByteArrayConstant(Blake2b256(x))
+    //backer's prover with his private key
+    val userProver = new ErgoProvingInterpreter
 
-    val height1 = 100000
-    val height2 = 50000
+    val regScript = userProver.dlogSecrets.head.publicImage
 
-    val deadlineA = 1000
-    val deadlineB = 500
-
-    Map[String, Any](
-      "height1" -> height1,
-      "height2" -> height2,
-      "deadlineA" -> deadlineA,
-      "deadlineB" -> deadlineB,
-      "pubkeyA" -> pubkeyA,
-      "pubkeyB" -> pubkeyB,
-      "hx" -> hx)
+    val env = Map(
+      "demurragePeriod" -> demurragePeriod,
+      "demurrageCost" -> demurrageCost,
+      "regScript" -> regScript
+    )
+    env
   }
 }
